95a54d7c450bcab0c81edca0ded902ecc5fe05eb
Tek Raj <ranatekraj06@gmail.com>
2025-06-11 10:11:47 +0530

    create get api for messages and users

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import express, { json } from 'express'
+import express from 'express'
 import mongoose from 'mongoose'
 import cors from 'cors'
 import http from "http"
@@ -6,6 +6,7 @@ import dotenv from "dotenv"
 import authRoutes from "./routes/auth.js"
 import { Server } from 'socket.io'
 import Messages from './models/Messages.js'
+import User from './models/User.js'
 
 dotenv.config()
 
@@ -31,10 +32,43 @@ io.on("connection", (socket) => {
         const {sender, receiver, message} = data;
         const newMessage = new Messages({sender, receiver, message});
         await newMessage.save()
+        socket.broadcast.emit("receive_message", data);
+    });
+
+
+    socket.on("disconnect", () => {
+        console.log("User disconnected", socket.id)
     })
 });
 
+app.get("/messages", async(req, res) => {
+    const {sender, receiver} = req.query;
 
+    try{
+
+        const message = await Messages.find({
+            $or: [
+                {sender, receiver},
+                {sender: receiver, receiver:sender}
+            ]
+        }).sort({createdAt: 1})
+        res.json(message)
+
+    }catch(err){
+        res.status(500).json({message: "Error fetching messages."})
+    }
+})
+
+app.get("/users", async(req, res) => {
+    const {currentUser} = req.query;
+
+    try{
+        const users = await User.find({username: {ne: currentUser}})
+        res.json(users)
+    }catch(err){
+        res.status(500).json({message: "Error fetching users."})
+    }
+})
 
 const PORT = process.env.PORT || 3000
 
